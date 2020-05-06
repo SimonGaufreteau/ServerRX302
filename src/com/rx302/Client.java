18ea90f98424ff39b155024dcf502decb734e92f
SimonGaufreteau <simon.gaufreteau@gmail.com>
2020-05-06 16:18:48 +0200

    Code for the Client

---
+++ src/com/rx302/Client.java
@@ -0,0 +1,35 @@
+package com.rx302;
+
+import java.util.Date;
+import java.util.Scanner;
+
+public class Client extends UDPInterface implements Runnable{
+	public Client(){
+		super();
+	}
+
+	@Override
+	public void run() {
+		send(CONNECT_MESSAGE,serverAddress,UDP_PORT);
+		do{
+			receive();
+		}while(!getMessageFromBuffer().equals(SERVER_RESPONSE));
+		String input="";
+		Scanner scanner = new Scanner(System.in);
+		while (!input.equals(DISCONNECT_CLIENT)){
+			System.out.println("Please enter the message to be sent to the server :");
+			input=scanner.nextLine();
+			send(input,serverAddress,UDP_PORT);
+			receive();
+			Date date = new Date(System.currentTimeMillis());
+			System.out.println(String.format(MESSAGE_RECEIVED,getSenderAddress(),getSenderPort(),date,getMessageFromBuffer()));
+		}
+		close();
+		System.out.println("Client disconnected successfully !");
+	}
+
+	public static void main(String[] args) {
+		Client client = new Client();
+		client.run();
+	}
+}
